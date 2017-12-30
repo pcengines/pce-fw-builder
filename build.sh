b6bff58712072585fc0facb8eedfc935838d1dd4
Piotr Król <piotr.krol@3mdeb.com>
2017-12-30 18:18:08 +0100

    simplify flow and use volumes instead of dockerfile add for pcw-fw-builder.sh
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- build.sh
+++ build.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
-    echo "Usage: ./build.sh <tag|branch> <platform> [<menuconfig-param>]"
-    echo "  <tag|branch> - tag or branch from github.com/pcengines/coreboot"
+if [ $# -lt 3 ]; then
+    echo "Usage: ./build.sh dev-build <path> <platform> [<menuconfig-param>]"
+    echo "  ./build.sh release <tag|branch> <platform> [<menuconfig-param>]"
+    echo "  dev-build - build source from <path>"
+    echo "  release - pull source and build according to <tag>"
+    echo "  <path> - full path to coreboot source"
+    echo "  <tag|branch> - tag or branch published on github.com/pcengines/coreboot"
     echo "  <platform> - apu2, apu3, apu4 or apu5"
     echo "  <menuconfig-param> - menuconfig interface, give 'help' for more information"
     exit
 fi
 
-if [ ! -d coreboot ]; then
-    git clone https://github.com/pcengines/coreboot.git -b coreboot-sdk-support
+if [ "$1" == "dev-build" ];then
+    # remove dev-build from options
+    shift
+
+    cb_path=$1
+
+    # remove coreboot path
+    shift
+    docker run --rm -it -v $cb_path:/home/coreboot/coreboot -v $PWD/scripts:/home/coreboot/scripts pcengines/pce-fw-builder:latest \
+        /home/coreboot/scripts/pce-fw-builder.sh $*
+elif [ "$1" == "release" ]; then
+    if [ -d release ]; then
+        sudo rm -rf release
+    fi
+
+    # remove release from options
+    shift
+    mkdir release
+    git clone https://github.com/pcengines/coreboot.git -b $2
+
+    # remove tag|branch from options
+    shift
+    cd coreboot
+    git submodule update --init --checkout
+    cd ..
+    docker run --rm -it -v $PWD/release/coreboot:/home/coreboot/coreboot -v $PWD/scripts:/home/coreboot/scripts pcengines/pce-fw-builder:latest \
+        /home/coreboot/scripts/pce-fw-builder.sh $*
 fi
 
-cd coreboot && git checkout $1
-git submodule update --init --checkout
-cd ..
-
-# docker pull pcengines/pce-fw-builder:latest
-docker run --rm -it -v $PWD/coreboot:/home/coreboot/coreboot pcengines/pce-fw-builder:latest \
-    /home/coreboot/scripts/pce-fw-builder.sh $*
--- scripts/pce-fw-builder.sh
+++ scripts/pce-fw-builder.sh
@@ -4,6 +4,6 @@ echo $*
 
 cd /home/coreboot/coreboot
 make distclean
-cp configs/pcengines_$2.config .config
+cp configs/pcengines_$1.config .config
 make -j$(nproc)
 
