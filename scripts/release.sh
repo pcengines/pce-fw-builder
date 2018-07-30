3a5b36838dcfcf16be0571001638bf7b959b1766
Adam Wawrzyński <adam.wawrzynski@3mdeb.com>
2018-07-30 11:25:50 +0200

    build and update docker image if Dockerfile differs
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@3mdeb.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,18 +2,7 @@
 
 USERNAME="pcengines"
 IMAGE="pce-fw-builder"
-
-# verify arguments
-if [[ "$1" =~ [0-9]+\.[0-9]+\.[0-9]+ ]] ;then
-    echo "Dockerfile tag: " $1
-else
-    echo "Invalid argument"
-    echo
-    echo "Usage: release.sh <tag>"
-    echo
-    echo "    <tag>    tag describing dockerfiles"
-    exit
-fi
+IMAGE_LEGACY=$IMAGE-legacy
 
 function errorCheck {
     ERROR_CODE="${?}"
@@ -23,30 +12,68 @@ function errorCheck {
     fi
 }
 
-# for testing
-exit
-
 # ensure we're up to date
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+CURRENT_BRANCH="$(git rev-parse HEAD)"
 git pull origin $CURRENT_BRANCH
 errorCheck "Failed to pull $CURRENT_BRANCH"
 
-# build mainline
-docker build -t pcengines/pce-fw-builder -f Dockerfile.ml .
-errorCheck "Build mainline failed"
+MAINLINE_BUILD="false"
+LEGACY_BUILD="false"
 
-# build legacy
-docker build -t pcengines/pce-fw-builder-legacy -f Dockerfile.legacy .
-errorCheck "Build legacy failed"
+# get list of tags from most recent
+TAG=`git tag -l --sort=-v:refname`
 
-# push mainline
-docker push $USERNAME/$IMAGE:latest
-errorCheck "Failed to push container: \"USERNAME/$IMAGE:latest\""
-docker push $USERNAME/$IMAGE:$version
-errorCheck "Failed to push container: \"USERNAME/$IMAGE:$version\""
+for current_tag in $TAG ; do
+    echo "$current_tag:"
 
-# push legacy
-docker push $USERNAME/$IMAGE-legacy:latest
-errorCheck "Failed to push container: \"USERNAME/$IMAGE-legacy:latest\""
-docker push $USERNAME/$IMAGE-legacy:$version
-errorCheck "Failed to push container: \"USERNAME/$IMAGE-legacy:$version\""
+    if [ "$MAINLINE_BUILD" == "false" ]; then
+        MAINLINE_DIFF=`git diff $CURRENT_BRANCH $current_tag -- Dockerfile.ml`
+        if [ "$MAINLINE_DIFF" == "" ]; then
+            echo "    Mainline: Dockerfile.ml is the same as $current_tag"
+        else
+            echo "    Build docker image"
+
+            # change tag of latest docker image
+            docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$current_tag
+
+            # build mainline
+            docker build -t $USERNAME/$IMAGE -f Dockerfile.ml .
+            errorCheck "Build mainline failed"
+
+            # push mainline
+            docker push $USERNAME/$IMAGE:latest
+            errorCheck "Failed to push container: \"USERNAME/$IMAGE:latest\""
+            docker push $USERNAME/$IMAGE:$current_tag
+            errorCheck "Failed to push container: \"USERNAME/$IMAGE:$current_tag\""
+
+            MAINLINE_BUILD="true"
+        fi
+    fi
+
+    if [ "$LEGACY_BUILD" == "false" ]; then
+        LEGACY_DIFF=`git diff $CURRENT_BRANCH $current_tag -- Dockerfile.legacy`
+        if [ "$LEGACY_DIFF" == "" ]; then
+            echo "    Legacy: Dockerfile.legacy is the same as $current_tag"
+        else
+            echo "    Build docker image"
+
+            # change tag of latest docker image
+            docker tag $USERNAME/$IMAGE_LEGACY:latest $USERNAME/$IMAGE_LEGACY:$current_tag
+
+            # build legacy
+            docker build -t $USERNAME/$IMAGE_LEGACY -f Dockerfile.legacy .
+            errorCheck "Build legacy failed"
+
+            # for testing
+            exit
+
+            # push legacy
+            docker push $USERNAME/$IMAGE_LEGACY:latest
+            errorCheck "Failed to push container: \"USERNAME/$IMAGE_LEGACY:latest\""
+            docker push $USERNAME/$IMAGE_LEGACY:$current_tag
+            errorCheck "Failed to push container: \"USERNAME/$IMAGE_LEGACY:$current_tag\""
+
+            LEGACY_BUILD="true"
+        fi
+    fi
+done
