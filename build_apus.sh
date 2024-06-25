500fa43b30ea5fd09e09964d5ae2085da40d8cc2
Piotr Król <piotr.krol@3mdeb.com>
2024-06-25 23:56:07 +0200

    build_apus.sh: gather hashes
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- build_apus.sh
+++ build_apus.sh
@@ -6,8 +6,8 @@ PLATFORMS_OVERRIDE=$2
 # Check for platform config files and build list of target platforms
 platforms=()
 for i in {2..7}; do
-	if [ -f "$DASHARO_PATH/configs/config.pcengines_apu$i" ]; then
-		platforms+=("apu$i")
+	if [ -f "$DASHARO_PATH/configs/config.pcengines_seabios_apu$i" ]; then
+		platforms+=("seabios_apu$i")
 	fi
 done
 
@@ -29,10 +29,9 @@ for platform in "${platforms[@]}"; do
 		echo "Build failed for $platform with status code $status_code."
 		exit $status_code
 	fi
+	mv $DASHARO_PATH/build/coreboot.rom ${platform}.rom
+	sha256sum ${platform}.rom >${platform}.sha256sum
 done
 
 echo "All builds completed successfully."
-
-find "$DASHARO_PATH/build" -type f -exec sha256sum {} + >/tmp/iter_hashes
-cut -d" " -f1 /tmp/iter_hashes | sort | uniq >/tmp/iter_hash_only
-comm -3 /tmp/iter_hash_only /tmp/v4.19.0.1_hash_only | wc -l
+cat *.sha256sum
